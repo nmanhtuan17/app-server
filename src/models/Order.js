2919f2d777ba6d1db5d39f77822641e3646f0e07
unknown <manhtuan17403@gmail.com>
2023-09-30 15:08:15 +0700

    n

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose')
 
-const Order = mongoose.Schema({
-    product: { type: String, require: true },
+const Order = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    userId: { type: String, require: true },
+    customerId: { type: String, require: true },
     qty: { type: Number, require: true },
-    customer: { type: String, require: true },
     address: {
         country: String,
         city: String,
         district: String,
         street: String
     },
+    delivery_status: { type: String, default: "pending" },
+    subtotal: { type: Number, require: true },
+    total: { type: Number, require: true },
+    payment_status: { type: String, require: true},
+
     desc: String
 })
 
--- src/models/User.js
+++ src/models/User.js
@@ -4,8 +4,8 @@ const User = mongoose.Schema({
     fullname: {type: String, require: true},
     username: {type: String, require: true},
     password: {type: String, require: true},
-    email: {type: String, require: true},
-    phone: {type: Number, require: true},
+    email: {type: String, require: true, unique: true},
+    location: {type: String, default: "Ha noi"}
 })
 
 module.exports = mongoose.model('User', User)
